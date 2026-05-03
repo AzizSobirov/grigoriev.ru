f21c59d07a224c0465b120e1b397188c2133b56c
Aziz <azizsobirovhazarasp@gmail.com>
2026-05-03 21:06:30 +0500

    feat: add Directions block and corresponding page route

--- src/blocks/index.ts
+++ src/blocks/index.ts
@@ -1,3 +1,4 @@
+export { default as Directions } from "./Directions.astro";
 export { default as Hero } from "./Hero.astro";
 export { default as News } from "./News.astro";
 export { default as Post } from "./Post.astro";
