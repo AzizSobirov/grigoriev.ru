68ea4ff3fb2096481f6b7f2302a0761a9a0bb1bc
Aziz <azizsobirovhazarasp@gmail.com>
2026-05-03 13:19:45 +0500

    added new blocks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,17 @@
-import { defineConfig } from "astro/config";
-import viteConfig from "./vite.config.js";
+import { defineConfig } from 'astro/config'
+import viteConfig from './vite.config.js'
+import icon from 'astro-icon'
 
-const isProduction = import.meta.env.VITE_MODE == "production";
+const isProduction = import.meta.env.VITE_MODE == 'production'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [],
-  output: "static",
+  integrations: [icon()],
+  output: 'static',
   compressHTML: false,
   build: {
-    format: isProduction ? "file" : "directory",
-    assets: "assets",
+    format: isProduction ? 'file' : 'directory',
+    assets: 'assets',
   },
   vite: viteConfig,
-});
+})
